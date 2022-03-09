49cc54f3bb21b8f52cbcd16aeff5889bee339116
Martin <ka_hei_martin_kwok@brown.edu>
2022-03-09 17:09:40 -0600

    Update limit code

--- HNLprocessor/corrections.py
+++ HNLprocessor/corrections.py
@@ -44,6 +44,12 @@ def add_ctau_weight(weights,llp_ctau, ctau_old, ctau_new):
     weights.add("ctau",ak.firsts(np.exp(llp_ctau * (1/ctau_old-1/ctau_new))*(ctau_old/ctau_new)))
     return
 
+def add_nCluster_weight(weights,name, n_cluster, cls_eff_ratio=1):
+    eff = ak.ones_like(n_cluster) * cls_eff_ratio
+    #print("Adding ctau weight")
+    weights.add(name,np.power(eff,n_cluster))
+    return
+
 def load_xsection():
     return compiled['xsections']
 
@@ -53,10 +59,12 @@ def reweightXsec(ctau,mass):
     #mass in GeV
     #xsec in pb
     cof={
-        "1":9.51561675,
-        "2":6.04926165,
-        "4":2.55645182,
-        "7":-0.29378948,
-        "10":-2.11196473,
+        "1p0":9.51561675,
+        "2p0":6.04926165,
+        "4p0":2.55645182,
+        "4p5":1.95648942,
+        "5p0":1.42447854,
+        "7p0":-0.29378948,
+        "10p0":-2.11196473,
     }
     return np.exp(-1*np.log(ctau)+cof[mass])

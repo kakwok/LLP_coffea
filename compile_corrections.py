fix Wpt normalization

compile_corrections.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import json
 import gzip
-import uproot3 as uproot
+import uproot3 as uproot3
+import  uproot
 import numexpr
 import numpy as np
 from coffea import hist, lookup_tools
@@ -11,7 +12,7 @@ import warnings
 
 corrections = {}
 
-with uproot.open("metadata/pileUp_Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.root") as fin_pileup:
+with uproot3.open("metadata/pileUp_Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.root") as fin_pileup:
     norm = lambda x: x / x.sum()
     #print(fin_pileup["pileup"].values)
     #print(fin_pileup["pileup"].values.sum())
@@ -62,14 +63,16 @@ corrections['2018_pileupweight_puDown'] = pileup_corr_puDown
 with uproot.open("./metadata/WPT_v2.root") as f:
     wpt_LO_HNL = f['h_HNL']
     wpt_LO_WJ  = f['h_WJ']
-with uproot.open("./metadata/wp-13tev-cms.root") as f:
+with uproot3.open("./metadata/wp-13tev-cms.root") as f:
     wpt_NLO = f['s_qt']
 
 wpt_NLO_normalized = wpt_NLO.values/wpt_NLO.values.sum()
+wpt_LO_HNL_normalized = wpt_LO_HNL.values()/wpt_LO_HNL.values().sum()
+wpt_LO_WJ_normalized = wpt_LO_WJ.values()/wpt_LO_WJ.values().sum()
 
 #ptrange = slice(np.searchsorted(wpt_LO.edges, 25.), np.searchsorted(wpt_LO.edges, 800.) + 1)
-corrections['wpt'] = lookup_tools.dense_lookup.dense_lookup( wpt_NLO_normalized /wpt_LO_HNL.values , wpt_LO_HNL.edges)
-corrections['wpt_WJ'] = lookup_tools.dense_lookup.dense_lookup( wpt_NLO_normalized /wpt_LO_WJ.values , wpt_LO_WJ.edges)
+corrections['wpt'] = lookup_tools.dense_lookup.dense_lookup( wpt_NLO_normalized /wpt_LO_HNL_normalized , wpt_LO_HNL.axes[0].edges())
+corrections['wpt_WJ'] = lookup_tools.dense_lookup.dense_lookup( wpt_NLO_normalized /wpt_LO_WJ_normalized , wpt_LO_WJ.axes[0].edges())
 
 def read_xsections(filename):
     out = {}